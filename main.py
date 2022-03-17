aa7cfd62cdf635f453430019e13f7db109c3c78f
AryanMan12 <aryanaryan1212.ag@gmail.com>
2022-03-17 21:27:58 +0530

    window initialized

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import pygame
 
+WIN = pygame.display.set_mode((300, 300))
+run = True
+
+while run:
+    WIN
+    for event in pygame.event.Event:
+        if event == pygame.QUIT:
+            run = False
+            quit()
+    
