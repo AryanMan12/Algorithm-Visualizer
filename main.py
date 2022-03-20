da4edcac54408b5ab0840ef4fd0e23652f5c14ec
Hrishi-g <hrishikeshrgupta@gmail.com>
2022-03-20 21:00:07 +0530

    null

--- main.py
+++ main.py
@@ -4,25 +4,14 @@ HEIGHT=600
 WIDTH=900
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 
-REGION_HEIGHT = 450
-REGION_WIDTH = 800
-REGION_COLOR = (255, 255, 255)
 
-LIST = [12,2,6,57,13,78,36,78,10]
-
-def rectangle():
-    pass
-
-def draw():
-    region = pygame.Rect(50, 100, REGION_WIDTH, REGION_HEIGHT)
-    pygame.draw.rect(WIN, REGION_COLOR, region)
-    pygame.display.update()
+# Aaya kya ❤️de
+#Emoji kaise use kiya J2
 
 def main():
     run = True
     clock=pygame.time.Clock()
 
-    draw()
     while run:
         clock.tick(60)
         for event in pygame.event.get():
@@ -30,7 +19,7 @@ def main():
                 run = False
             
     pygame.quit()
-
+                
 
 if __name__ == "__main__":
     main()       
