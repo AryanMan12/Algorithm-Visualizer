7b27b86cf2552605c1611b5b44e125425e98b745
AryanMan12 <aryanaryan1212.ag@gmail.com>
2022-03-21 22:19:02 +0530

    Visualization Of Bubble and Insertion Sort

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import math
 import pygame
 import random
 
-LIST = [12,2,6,57,13,78,36,78,10,45,35,2,45,3,22,3,2,4,35,66,23,14,15]
+LIST = [12,2,6,57,13,78,36,70,10,45,35,2,45,3,22,3,2,4,35,66,23,14,15,30]
 
 HEIGHT=600
 WIDTH=900
@@ -11,6 +11,9 @@ WHITE = (240,240,240)
 LIGHT_GREY = (192,192,192)
 MEDIUM_GREY = (128,128,128)
 DARK_GREY = (64,64,64)
+RED = (255,0,0)
+GREEN = (116,161,66)
+
 
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 
@@ -28,21 +31,49 @@ REGION_COLOR = WHITE
 NUM_OF_ELEMENTS = len(LIST)
 MAX_VAL = max(LIST)
 RECTANGLE_WIDTH = math.ceil(REGION_WIDTH/NUM_OF_ELEMENTS)
-UNIT_HEIGHT = REGION_HEIGHT//MAX_VAL
+UNIT_HEIGHT = math.ceil(REGION_HEIGHT/MAX_VAL)
 
+sorting = False
 
-def rectangle():
-    for i,num in enumerate(LIST):
-        element_padding_height = PADDING_TOP+(REGION_HEIGHT-(UNIT_HEIGHT*num))
-        element_padding_width = PADDING_LEFT+(i*RECTANGLE_WIDTH)
-        element_rect = pygame.Rect(element_padding_width, element_padding_height, RECTANGLE_WIDTH, UNIT_HEIGHT*num)
-        pygame.draw.rect(WIN, LIST_COLORS[i%3], element_rect)
 
 def bubble_sort():
     for i in range(len(LIST)):
         for j in range(0, len(LIST) - i - 1):
             if LIST[j] > LIST[j + 1]:
                 LIST[j],LIST[j + 1]=LIST[j + 1],LIST[j]
+                rectangle({j: RED, j+1:GREEN}, True)
+                yield True
+    return LIST
+
+def insertionSort():
+    for i in range(1, len(LIST)):
+        key = LIST[i]
+        j = i-1
+        while j >=0 and key < LIST[j] :
+            LIST[j+1] = LIST[j]
+            j -= 1
+            rectangle({j: GREEN, j+1:RED}, True)
+            yield True
+        LIST[j+1] = key
+    return LIST
+
+def rectangle(cols={}, clear = False):
+
+    if clear:
+        clear_rect = (PADDING_LEFT-10, PADDING_TOP-20, REGION_WIDTH+20, REGION_HEIGHT+25)
+        pygame.draw.rect(WIN, WHITE, clear_rect)
+
+    for i,num in enumerate(LIST):
+        element_padding_height = PADDING_TOP+(REGION_HEIGHT-(UNIT_HEIGHT*num))
+        element_padding_width = PADDING_LEFT+(i*RECTANGLE_WIDTH)
+        element_rect = pygame.Rect(element_padding_width, element_padding_height, RECTANGLE_WIDTH, UNIT_HEIGHT*num)
+        color = LIST_COLORS[i%3]
+        if i in cols:
+            color = cols[i]
+        pygame.draw.rect(WIN, color, element_rect)
+        
+    if clear:
+        pygame.display.update()
 
 def draw():
     WIN.fill(WHITE)
@@ -51,14 +82,28 @@ def draw():
 
 def main():
     run = True
+    sorting = False
+    sorting_algorithm = None
     clock=pygame.time.Clock()
     
     while run:
-        draw()
-        clock.tick(60)
+        if sorting:
+            try:
+                next(sorting_algorithm)
+            except StopIteration:
+                sorting = False
+        else:
+            draw()
+        clock.tick(5)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            if event.type != pygame.KEYDOWN:
+                continue
+            if event.key == pygame.K_SPACE and sorting == False:
+                sorting = True
+                sorting_algorithm = insertionSort()
+
             
     pygame.quit()
 
