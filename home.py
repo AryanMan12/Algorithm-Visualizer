6cdf69be116be5a5f25ac942e8aab5d53271f126
AryanMan12 <aryanaryan1212.ag@gmail.com>
2022-03-29 21:27:41 +0530

    home base created

--- home.py
+++ home.py
@@ -1,1 +1,14 @@
 from tkinter import *
+from sorting_menu import *
+
+WIN = Tk()
+WIN.title("Algorithm Visualiser")
+WIN.geometry("900x600")
+
+def onSorting():
+    onSortingCall(WIN)
+
+sorting_btn = Button(WIN, text="Sorting", command=onSorting)
+sorting_btn.grid(row=10, column=10)
+
+WIN.mainloop()
--- sorting_menu.py
+++ sorting_menu.py
@@ -1,5 +1,4 @@
 from tkinter import *
-WIN1=Tk()
-WIN1.geometry("900x600")
 
-WIN1.mainloop()
+def onSortingCall(WIN):
+    pass
